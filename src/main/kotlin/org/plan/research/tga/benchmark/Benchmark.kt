7b8042b0de65d5dbf3da60f1dc121f4b33660184
Azat Abdullin <azat.aam@gmail.com>
2024-03-14 17:03:09 +0100

    m

--- src/main/kotlin/org/plan/research/tga/benchmark/Benchmark.kt
+++ src/main/kotlin/org/plan/research/tga/benchmark/Benchmark.kt
@@ -1,16 +1,20 @@
 package org.plan.research.tga.benchmark
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 import org.plan.research.tga.benchmark.json.ListOfPathSerializer
 import org.plan.research.tga.benchmark.json.PathAsStringSerializer
 import java.nio.file.Path
 
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class Benchmark(
     val name: String,
     @Serializable(with = PathAsStringSerializer::class)
     val root: Path,
-    val build_id: String,
+    @JsonNames("build_id", "buildId")
+    val buildId: String,
     @Serializable(with = ListOfPathSerializer::class)
     val classPath: List<Path>,
     val klass: String,
