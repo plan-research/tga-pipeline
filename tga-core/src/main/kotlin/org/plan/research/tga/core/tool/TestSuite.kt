ec0c05ff3e03ee11d6538a36bd518aec720a5731
Azat Abdullin <azat.aam@gmail.com>
2024-04-10 17:02:28 +0200

    fix #2: remove incorrect custom serializer

--- tga-core/src/main/kotlin/org/plan/research/tga/core/tool/TestSuite.kt
+++ tga-core/src/main/kotlin/org/plan/research/tga/core/tool/TestSuite.kt
@@ -1,7 +1,6 @@
 package org.plan.research.tga.core.tool
 
 import kotlinx.serialization.Serializable
-import org.plan.research.tga.core.benchmark.json.ListOfPathSerializer
 import org.plan.research.tga.core.benchmark.json.PathAsStringSerializer
 import org.plan.research.tga.core.dependency.Dependency
 import java.nio.file.Path
@@ -11,6 +10,5 @@ data class TestSuite(
     @Serializable(with = PathAsStringSerializer::class)
     val testSrcPath: Path,
     val tests: List<String>,
-    @Serializable(with = ListOfPathSerializer::class)
     val dependencies: List<Dependency>,
 )
