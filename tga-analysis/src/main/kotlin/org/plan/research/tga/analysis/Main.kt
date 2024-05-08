1f93dfe0eff5e8f725b029ceecef36bbffb2b6da
Azat Abdullin <azat.aam@gmail.com>
2024-05-08 15:40:46 +0200

    m

--- tga-analysis/src/main/kotlin/org/plan/research/tga/analysis/Main.kt
+++ tga-analysis/src/main/kotlin/org/plan/research/tga/analysis/Main.kt
@@ -6,22 +6,20 @@ import org.vorpal.research.kthelper.logging.log
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
     val results = getJsonSerializer(pretty = true).decodeFromString<List<ToolResults>>(File(args[0]).readText())
-
-    log.debug("results: {}", results)
+    val resultsBranches = results.filter { it.coverage.branches.total > 0U }
 
     log.debug(
         "Average coverage: {} {}",
         results.sumOf { it.coverage.lines.ratio } / results.size,
-        results.sumOf { it.coverage.branches.ratio } / results.size
+        resultsBranches.sumOf { it.coverage.branches.ratio } / resultsBranches.size
     )
 
-    val filteredResults = results.filter { it.coverage.lines.ratio > 0.0 }
+    val filteredLines = results.filter { it.coverage.lines.ratio > 0.0 }
+    val filteredBranches = resultsBranches.filter { it.coverage.branches.ratio > 0.0 }
     log.debug(
         "Average filtered coverage: {} {}",
-        filteredResults.sumOf { it.coverage.lines.ratio } / filteredResults.size,
-        filteredResults.sumOf { it.coverage.branches.ratio } / filteredResults.size
+        filteredLines.sumOf { it.coverage.lines.ratio } / filteredLines.size,
+        filteredBranches.sumOf { it.coverage.branches.ratio } / filteredBranches.size
     )
 }
