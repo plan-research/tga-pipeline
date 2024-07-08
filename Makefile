8c68bd1771d529be18fcb06cd113fb6ed42dfbf4
Azat Abdullin <azat.aam@gmail.com>
2024-07-08 17:35:51 +0200

    version up

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-VERSION = 0.0.16
+VERSION = 0.0.17
 
 .PHONY: benchmarks all publish
 
--- buildSrc/src/main/kotlin/org.plan.research.tga-pipeline-base.gradle.kts
+++ buildSrc/src/main/kotlin/org.plan.research.tga-pipeline-base.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.plan.research"
-version = "0.0.16"
+version = "0.0.17"
 
 repositories {
     mavenCentral()
