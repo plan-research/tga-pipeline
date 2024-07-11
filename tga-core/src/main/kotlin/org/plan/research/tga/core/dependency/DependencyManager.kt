96cec32c90254135bed16fc6463b56e0d3a219ed
Azat Abdullin <azat.aam@gmail.com>
2024-07-11 12:22:06 +0200

    make DependencyManager synchronized

--- tga-core/src/main/kotlin/org/plan/research/tga/core/dependency/DependencyManager.kt
+++ tga-core/src/main/kotlin/org/plan/research/tga/core/dependency/DependencyManager.kt
@@ -7,12 +7,15 @@ import dev.jeka.core.api.depmanagement.resolution.JkResolutionParameters
 import java.nio.file.Path
 
 class DependencyManager {
+    private val lock = Any()
     private val loadedDependencies = mutableMapOf<Dependency, List<Path>>()
 
-    fun findDependency(dependency: Dependency): List<Path> = loadedDependencies.getOrPut(dependency) {
-        val deps = JkDependencySet.of()
-            .and("${dependency.groupId}:${dependency.artifactId}:${dependency.version}")
-        val resolver = JkDependencyResolver.of(JkRepo.ofMavenCentral());
-        resolver.resolve(deps, JkResolutionParameters.of()).files.entries
+    fun findDependency(dependency: Dependency): List<Path> = synchronized(lock) {
+        loadedDependencies.getOrPut(dependency) {
+            val deps = JkDependencySet.of()
+                .and("${dependency.groupId}:${dependency.artifactId}:${dependency.version}")
+            val resolver = JkDependencyResolver.of(JkRepo.ofMavenCentral());
+            resolver.resolve(deps, JkResolutionParameters.of()).files.entries
+        }
     }
 }
