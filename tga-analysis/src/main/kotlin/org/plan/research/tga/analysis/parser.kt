9307c92302302c18a7dfb485d935f6f9c866c3a6
Azat Abdullin <azat.aam@gmail.com>
2024-07-23 16:43:17 +0200

    m

--- tga-analysis/src/main/kotlin/org/plan/research/tga/analysis/parser.kt
+++ tga-analysis/src/main/kotlin/org/plan/research/tga/analysis/parser.kt
@@ -19,28 +19,34 @@ fun main(args: Array<String>) {
     Files.walk(resultsDir).forEach { file: Path ->
         if (file.toString().endsWith(".java")) {
             val compilationUnit = tryOrNull { StaticJavaParser.parse(file) } ?: return@forEach
-            compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java).forEach { classDeclaration ->
-                classDeclaration.findAll(MethodDeclaration::class.java).forEach { methodDeclaration ->
-                    methodDeclaration.findAll(TryStmt::class.java).forEach { tryStmt ->
-                        when (val parent = tryStmt.parentNode.get()) {
-                            is BlockStmt -> {
-                                val index = parent.statements.indexOf(tryStmt)
-                                parent.statements.removeAt(index)
-                                for (subNode in tryStmt.tryBlock.childNodes.reversed()) {
-                                    if (subNode is Statement) {
-                                        parent.statements.add(index, subNode)
-                                    } else if (subNode is LineComment) {
-                                        continue
-                                    } else {
-                                        throw IllegalStateException("Unexpected element in try block: $subNode")
+            compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java)
+                .forEach { classDeclaration ->
+                    classDeclaration.findAll(MethodDeclaration::class.java)
+                        .filter { methodDeclaration -> methodDeclaration.annotations.any { it.name.identifier == "Test" } }
+                        .forEach { methodDeclaration ->
+                            methodDeclaration.thrownExceptions.add(StaticJavaParser.parseClassOrInterfaceType("java.lang.Throwable"))
+                            methodDeclaration.findAll(TryStmt::class.java)
+                                .forEach { tryStmt ->
+                                    when (val parent = tryStmt.parentNode.get()) {
+                                        is BlockStmt -> {
+                                            val index = parent.statements.indexOf(tryStmt)
+                                            parent.statements.removeAt(index)
+                                            for (subNode in tryStmt.tryBlock.childNodes.reversed()) {
+                                                if (subNode is Statement) {
+                                                    parent.statements.add(index, subNode)
+                                                } else if (subNode is LineComment) {
+                                                    continue
+                                                } else {
+                                                    throw IllegalStateException("Unexpected element in try block: $subNode")
+                                                }
+                                            }
+                                        }
+
+                                        else -> throw IllegalStateException("Unexpected parent: $parent")
                                     }
                                 }
-                            }
-                            else -> throw IllegalStateException("Unexpected parent: $parent")
                         }
-                    }
                 }
-            }
             file.bufferedWriter().use {
                 it.write(compilationUnit.toString())
             }
