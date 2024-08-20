d2d8009e7bc294337fa2f03ddea631f300f41d88
Azat Abdullin <azat.aam@gmail.com>
2024-08-20 15:42:11 +0200

    m

--- buildSrc/src/main/kotlin/org.plan.research.tga-pipeline-base.gradle.kts
+++ buildSrc/src/main/kotlin/org.plan.research.tga-pipeline-base.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.plan.research"
-version = "0.0.21"
+version = "0.0.22"
 
 repositories {
     mavenCentral()
--- scripts/execute_benchmark.py
+++ scripts/execute_benchmark.py
@@ -12,8 +12,8 @@ from generate_compose import Tool
 from generate_compose import generate_compose
 
 # Global parameters
-RUNNER_IMAGE = "abdullin/tga-pipeline:runner-0.0.21"
-TOOL_IMAGE = "abdullin/tga-pipeline:tools-0.0.21"
+RUNNER_IMAGE = "abdullin/tga-pipeline:runner-0.0.22"
+TOOL_IMAGE = "abdullin/tga-pipeline:tools-0.0.22"
 BENCHMARKS_FILE = "/var/benchmarks/gitbug/benchmarks.json"
 
 
